7c7ec06f62b58f0931992b17cf25dbef52ba730f
alli93 <alexanderb13@ru.is>
2017-12-12 11:18:19 -0800

    Added JUnit reporter

--- server/jasmine-setup.js
+++ server/jasmine-setup.js
@@ -10,3 +10,11 @@ let consoleReporter = new JasmineConsoleReporter({
     activity: false
 });
 jasmine.getEnv().addReporter(consoleReporter);
+
+
+var reporters = require('jasmine-reporters');
+var junitReporter = new reporters.JUnitXmlReporter({
+    savePath: './jasmine-reports/',
+    consolidateAll: false
+});
+jasmine.getEnv().addReporter(junitReporter)
