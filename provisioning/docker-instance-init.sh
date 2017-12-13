057612239fd2b7153c4c5a92d178c0e11f2b9a98
alli93 <alexanderb13@ru.is>
2017-12-13 07:26:04 -0800

    Added git install to docker instance

--- provisioning/docker-instance-init.sh
+++ provisioning/docker-instance-init.sh
@@ -7,6 +7,8 @@ sudo yum -y install docker
 sudo pip install docker-compose
 sudo pip install backports.ssl_match_hostname --upgrade
 
+sudo yum -y install git
+
 sudo service docker start
 sudo usermod -a -G docker ec2-user
 
